512a59df34443e0b82f1f62cb315797194af6d08
Sehyo Chang <sehyo@fluvio.io>
2022-08-14 16:45:15 -0700

    migrate smartmodule store

--- crates/fluvio-spu/src/smartengine/mod.rs
+++ crates/fluvio-spu/src/smartengine/mod.rs
@@ -14,7 +14,7 @@ use fluvio::{ConsumerConfig};
 
 use futures_util::{StreamExt, stream::BoxStream};
 
-use crate::core::DefaultSharedGlobalContext;
+use crate::core::{DefaultSharedGlobalContext, smartmodule_localstore};
 
 pub struct SmartModuleContext {
     pub smartmodule_instance: Box<dyn SmartModuleInstance>,
@@ -145,7 +145,7 @@ impl SmartModuleContext {
         // then get smartmodule context
         let payload = match invocation.wasm {
             SmartModuleInvocationWasm::Predefined(name) => {
-                if let Some(smartmodule) = ctx.smartmodule_localstore().spec(&name) {
+                if let Some(smartmodule) = smartmodule_localstore().spec(&name) {
                     let wasm = SmartModuleWasmCompressed::Gzip(smartmodule.spec.wasm.payload);
                     LegacySmartModulePayload {
                         wasm,
