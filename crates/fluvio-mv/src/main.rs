7522900ea704777b48cd3a19d817ce207a42255c
Sehyo Chang <sehyo@fluvio.io>
2023-05-03 15:44:48 -0700

    add fmv

--- crates/fluvio-mv/src/main.rs
+++ crates/fluvio-mv/src/main.rs
@@ -3,6 +3,6 @@ use clap::Parser;
 fn main() {
     fluvio_future::subscriber::init_tracer(None);
 
-   // let opt = fluvio_spu::SpuOpt::parse();
-    //fluvio_spu::main_loop(opt);
+    // let opt = fluvio_spu::SpuOpt::parse();
+    //  fluvio_spu::main_loop(opt);
 }
